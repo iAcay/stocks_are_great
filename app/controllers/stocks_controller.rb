9fb0e09fde4dff9a2a29d6c0122733d0acf34fd8
iAcay <96024046+iAcay@users.noreply.github.com>
2022-06-14 23:21:12 +0200

    feat: handling empty params and not valid symbols

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,15 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        redirect_to my_portfolio_path, alert: 'Not found entered symbol :('
+      end
+    else
+      redirect_to my_portfolio_path, alert: 'This field cannot be empty.'
+    end
   end
 end
