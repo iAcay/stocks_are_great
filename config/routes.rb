861f9e6ece59884f2c421d6a1e9a6b398401c9ba
iAcay <96024046+iAcay@users.noreply.github.com>
2022-06-29 18:42:23 +0200

    feat: create many to many association between users and stocks

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   root to: "welcome#index"
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+
+  resources :user_stocks
 end
