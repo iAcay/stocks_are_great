a3fafcdf1ac865407749e2fd26046136c3e9e5f7
iAcay <96024046+iAcay@users.noreply.github.com>
2022-06-30 14:56:06 +0200

    feat: introduce possibility to untrack stocks

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,11 @@ class UserStocksController < ApplicationController
     @user_stock = UserStock.create(user: current_user, stock: stock)
     redirect_to my_portfolio_path, notice: "Stock #{stock.name} was successfully added to your portfolio."
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    redirect_to my_portfolio_path, notice: "#{stock.ticker} was successfully removed from portfolio."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
 
-  resources :user_stocks, only: %i[create]
+  resources :user_stocks, only: %i[create destroy]
 end
